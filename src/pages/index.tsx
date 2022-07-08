c14ba52bacfe09fd8922ec8371ce377caa9f5116
João Andrade <joaovictorvieira.23@hotmail.com>
2022-07-08 18:29:20 -0300

    feat: set header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, VStack, IconButton } from '@chakra-ui/react';
 import AddTodo from '../components/AddTodo';
 import TodoList from '../components/TodoList';
-
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 export default function Home() {
   return (
     // AddTodo
     // TodoList
-    <Heading>Todo Application</Heading>
+    <VStack p={4}>
+      <IconButton icon={<FaSun />} isRound="true" size="lg" alignSelf="flex-end"/>
+      <Heading 
+        mb="8" 
+        fontWeight="extrabold" 
+        size="2xl" 
+        bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+        bgClip="text"
+        >
+          Todo Application
+        </Heading>
+    </VStack>
   )
 }
